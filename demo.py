1825a583c5e277ae060e24c312a467545abcf39e
chao-ji <jic@indiana.edu>
2017-09-01 02:37:34 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,123 +1,44 @@
-from test import *
 import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+from autodiff import *
 
-"""
-Inspired by
-https://cs224d.stanford.edu/notebooks/vanishing_grad_example.html
-"""
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-np.random.seed(0)
-N = 100 # number of points per class
-D = 2 # dimensionality
-K = 3 # number of classes
-X = np.zeros((N*K,D))
-num_train_examples = X.shape[0]
-y = np.zeros(N*K, dtype='uint8')
-
-for j in xrange(K):
-    ix = range(N*j,N*(j+1))
-    r = np.linspace(0.0,1,N) # radius
-    t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
-    X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
-    y[ix] = j
-
-h=50
-h2=50
-num_train_examples = X.shape[0]
-
-model={}
-model['h'] = h # size of hidden layer 1
-model['h2']= h2# size of hidden layer 2
-model['W1']= 0.1 * np.random.randn(D,h)
-model['b1'] = np.zeros((1,h))
-model['W2'] = 0.1 * np.random.randn(h,h2)
-model['b2']= np.zeros((1,h2))
-model['W3'] = 0.1 * np.random.randn(h2,K)
-model['b3'] = np.zeros((1,K))
-
-h= model['h']
-h2= model['h2']
-W1= model['W1']
-W2= model['W2']
-W3= model['W3']
-b1= model['b1']
-b2= model['b2']
-b3= model['b3']
-
-step_size=1e-1
-
-x = PlaceholderOp([300, 2])
-w1 = PlaceholderOp([2, 50])
-
-i1 = PlaceholderOp([300, 1])
-bb1 = PlaceholderOp([1, 50])
-
-a1l = MulOp(x, w1)
-a1r = MulOp(i1, bb1)
-
-h_raw = AddOp(a1l, a1r)
-
-h = SigmoidOp(h_raw)
-
-#h = PlaceholderOp([300, 50])
-w2 = PlaceholderOp([50, 50])
-
-i2 = PlaceholderOp([300, 1])
-bb2 = PlaceholderOp([1, 50])
-
-a2l = MulOp(h, w2)
-a2r = MulOp(i2, bb2)
-
-h2_raw = AddOp(a2l, a2r) 
-
-#h2_raw = PlaceholderOp([300, 50])
-h2 = SigmoidOp(h2_raw) 
-
-
-
-
-w3 = PlaceholderOp([50, 3])
-i3 = PlaceholderOp([300, 1])
-bb3 = PlaceholderOp([1, 3])
-
-a3l = MulOp(h2, w3)
-a3r = MulOp(i3, bb3)
-s = AddOp(a3l, a3r)
-
-H = SoftmaxCrossEntropyWithLogitsOp(s, y)
-
+batch_size = 10000
 reg = 1e-3
+iterations = 1000
 
-r_w1 = RegMatOp(w1, reg)
-r_w2 = RegMatOp(w2, reg)
-r_w3 = RegMatOp(w3, reg)
+w = np.random.normal(scale=0.01, size=(784, 10))
+b = np.random.normal(scale=0.01, size=(1, 10))
 
-ll = AddOp(AddOp(H, r_w1), AddOp(r_w2, r_w3))
+sess = Session()
 
+X = PlaceholderOp([batch_size, 784], sess, False)
+Y = PlaceholderOp([batch_size, 10], sess, False)
+W = PlaceholderOp([784, 10], sess)
+B = PlaceholderOp([10], sess)
+S = AddOp(MatMulOp(X, W, sess), BiasBroadcastOp(B, [batch_size, 10], sess), sess)
+H = ReduceMeanOp(SoftmaxCrossEntropyWithLogitsOp(Y, S, sess), 0, sess)
+F = AddOp(H, ParamRegOp(W, reg, sess), sess)
 
-for ii in range(1):
-    feed_dict = {   w3: W3,
-                i3: np.ones((300, 1)),
-                bb3: b3,
-                w2: W2,
-                i2: np.ones((300, 1)),
-                bb2: b2,
-                w1: W1,
-                i1: np.ones((300, 1)),
-                bb1: b1,
-                x: X }
+feed_dict = {W: w, B: b}
+params = {"alpha": 1e-3}
+for ii in xrange(iterations):
+  batch_xs, batch_ys = mnist.train.next_batch(batch_size)
 
-    print ll.eval(feed_dict)
-    ll.deriv(feed_dict, np.ones((1, 1)))
+  feed_dict[X] = batch_xs
+  feed_dict[Y] = batch_ys
 
+  if ii % 100 == 0:
+    F_val = sess.eval_tensor(F, feed_dict)
+    S_val = sess.eval_tensor(S, feed_dict)
+    print "iteration: %d, loss: %f, train accuracy: %f" % (ii, F_val, np.mean(np.argmax(S_val, axis=1) == np.argmax(batch_ys, axis=1)))
 
-    W1 += -step_size * w1.adjoint
-    b1 += -step_size * bb1.adjoint
-    W2 += -step_size * w2.adjoint
-    b2 += -step_size * bb2.adjoint
-    W3 += -step_size * w3.adjoint
-    b3 += -step_size * bb3.adjoint
-    
-#    w3.reset(); w2.reset(); w1.reset(); bb3.reset(); bb2.reset(); bb1.reset()
-#    i3.reset(); i2.reset(); i1.reset(); x.reset(); 
+  sess.sgd_update(params, F, feed_dict)
 
+print "Test stage:"
+
+feed_dict[X] = mnist.test.images
+
+S_val = sess.eval_tensor(S, feed_dict)
+print "test set size: %d, test accuracy: %f" % (mnist.test.images.shape[0], np.mean(np.argmax(S_val, axis=1) == np.argmax(mnist.test.labels, axis=1)))
